bf58750fe3a2b0a636bca889a82e159a552ff488
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-08-15 17:51:50 -0300

    Rewriting machine DB structure.

--- install.py
+++ install.py
@@ -1,21 +1,25 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from datetime import datetime
-import requests, uuid, os
+import requests, warnings, uuid, sys, os
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings('ignore', category=CryptographyDeprecationWarning)
 
 def main():
   client = connect_db()
+  mac = get_mac_address()
   db_master = client['Ouroboros']
-  col_configs = db_master['Configs']
-
   db_machines = client['Machines']
 
+  col_configs = db_master['Configs']
   wh_alerts = col_configs.distinct('webhooks.wh_alerts')[0]
-  #wh_alerts = client['Ouroboros']['Ouroboros'].find_one({"webhook.wh_alerts": {"$exists": True}})
 
-  add_machine(db_machines)
-
-  #message(wh_alerts, f'Install: {mac}')
+  install = add_machine(db_machines, mac)
+  if install[0] == True:
+    print(install[-1])
+    #message(wh_alerts, f'New machine: {mac}')
+  else:    
+    print(install[-1])
 
 def connect_db():
   #TODO: Colocar URI fixa.
@@ -27,29 +31,31 @@ def message(webhook, message):
   headers = {'Content-Type': 'application/json'}
   requests.post(webhook, json={'content': message}, headers=headers)
 
-def add_machine(db_machines):
-  mac = get_mac_address()
-  if mac not in db_machines.list_collection_names():
-    db_machines[mac].insert_one(
-      {
-        'infos': {
-          'name': '',
-          'date_implanted': datetime.now(),
-          'machine_mac': mac
-        },
+def add_machine(db_machines, mac):
+  try:
+    if mac not in db_machines.list_collection_names():
+      db_machines[mac].insert_one(
+        {
+          'infos': {
+            'name': '',
+            'date_implanted': datetime.now(),
+            'machine_mac': mac
+          },
 
-        'configs': {
-          'prints': {
-            'webhook': '',
-            'auto_send' : False,
-            'delay': 5000
+          'configs': {
+            'prints': {
+              'webhook': '',
+              'auto_send' : False,
+              'delay': 5000
+            }
           }
         }
-      }
-    )
-    print('Machine Added Sucessfully!')
-  else:
-    print('Machine exists!')
+      )
+      return [True, 'Machine Added!']
+    else:
+      return [True, 'Machine Already Exists!']
+  except Exception as e:
+    return [False, e]
 
 def get_mac_address():
   try:
